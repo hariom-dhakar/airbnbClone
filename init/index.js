1d55635a467dc60c1613d00a0e15c4ccd0d8bc4f
Hariom <harudhakar@gmail.com>
2025-01-30 18:46:09 +0530

    first commit

--- init/index.js
+++ init/index.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../Models/listing.js");
+const mapboxToken = process.env.ACCESS_TOKEN;
+const geocodingClient = require("@mapbox/mapbox-sdk/services/geocoding")({
+  accessToken: mapboxToken,
+});
 
 const dbUrl = process.env.MONGODB_URI;
 
@@ -19,16 +23,29 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((ob) => ({
-    ...ob,
-    owner: "679b44a4a5a838e19ec88aa0",
-    geometry: {
-      type: ob.geometry?.type || "Point",
-      coordinates: ob.geometry?.coordinates || [0, 0],
-    },
-  }));
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
+
+  const updatedData = await Promise.all(
+    initData.data.map(async (ob) => {
+      const geocodeResponse = await geocodingClient
+        .forwardGeocode({
+          query: ob.location,
+          limit: 1,
+        })
+        .send();
+      const coordinates = geocodeResponse.body.features[0]?.geometry
+        ?.coordinates || [0, 0];
+      return {
+        ...ob,
+        owner: "679b44a4a5a838e19ec88aa0",
+        geometry: {
+          type: ob.geometry?.type || "Point",
+          coordinates,
+        },
+      };
+    })
+  );
+  await Listing.insertMany(updatedData);
+  console.log("Data was initialized");
 };
 
 initDB();
